c4bab35b443973529b0a52c7b128d7f73922841d
Anas <you@example.com>
2022-06-22 19:54:25 +0200

    add view

---
+++ src/view/Main.java
@@ -0,0 +1,17 @@
+package view;
+
+import controller.Controller;
+import java.io.FileNotFoundException;
+import model.InvoiceHeader;
+import model.InvoiceLine;
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        View view = new View();
+        view.setVisible(true);
+        view.setLocations();
+        view.setResizable(false);
+        view.loadFiles();
+
+        new Controller(new InvoiceHeader(), new InvoiceLine(), view);
+    }
+}
